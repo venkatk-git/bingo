9166bfcb131988ae7b97a206a545e6a15c21e960
venkatk-git <venkatkumar.profiles@gmail.com>
2025-03-01 19:37:43 +0530

    app: setup and configuration

---
+++ package/config/site.config.ts
@@ -0,0 +1,9 @@
+const config = {
+  name: 'bingo',
+  version: '0.0.0',
+  contributors: ['Venkat'],
+  gitRepo: 'https://github.com/venkatk-git/bingo',
+};
+
+export type Config = typeof config;
+export default config;
